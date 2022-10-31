26203dec046ea0565b9c82c0bbee145ec2bd30f3
xackerboydili <isoqovdilshod47@gmail.com>
2022-10-31 18:36:19 +0500

    if else masalalar

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// // if( yosh >= 18){
+// //     alert("siz balog'at yoshidasiz")
+// // }
+
+// //     else{
+//     //         alert("xali ozgina bor")
+//     // }
+
+// // switch( yosh ){
+// //     case 18:
+// //         alert("yoshingiz to'gri keladi")
+// //         break
+// //     case 19:
+// //         alert("yoshingiz to'gri keladi")
+// //         break
+// //     case 20:
+// //         alert("yoshingiz to'gri keladi")
+// //         break
+// //     case 21:
+// //         alert("yoshingiz to'gri kelmaydi")
+// //         break
+// // }
+// // if(yosh >= 22){
+//     //     alert("yoshingiz to'g'ri kelmaydi")
+//     // }
+//     //     else{
+//         //         alert("yoshingiz to'g'ri kelmaydi")
+//         //     }
+        
+        let yosh = prompt("yoshingizni kiriting");
+
+        let message;
+
+
+ message = Number(yosh) >= 18 ? alert("mashina xaydashingi mumkin") : Number(yosh) < 18? alert("Xali yoshsiz") : String(yosh)?alert('Xato'):alert('bunday yosh yoq');
+
+
+// // let sana = 0;
+
+// // do{
+// //     console.log(sana)
+// //     sana++;
+// // }
+// // while( sana >= 20 )
+    
+
+// function lessThanOrEqualToZero(num) {
+// 	let state;
+// 	if(num<=0){
+// 		state = true
+// 	}	
+// 	else{
+// 		state = false
+// 	}
+// 	return state
+    
+    
+// }
+// console.log(lessThanOrEqualToZero(-4))
+
+// let a=4;
+// a++;
+// console.log(++a)
+
+
